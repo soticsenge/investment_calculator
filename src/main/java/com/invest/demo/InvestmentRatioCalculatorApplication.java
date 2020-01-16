d412cac90e5bad58cd707ab5c8a383bb0fc59e9e
Csenge Soti <Csenge_Soti@epam.com>
2020-01-16 12:25:41 +0100

    Init project

---
+++ src/main/java/com/invest/demo/InvestmentRatioCalculatorApplication.java
@@ -0,0 +1,17 @@
+package com.invest.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class InvestmentRatioCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentRatioCalculatorApplication.class, args);
+    }
+}
